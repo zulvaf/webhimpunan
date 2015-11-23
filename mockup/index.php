ea47eab5e89ee5dde92c42d098bb2364c01bcc9f
Ahmad N. F <fahmadnaufal@gmail.com>
2015-11-23 09:21:09 +0700

    added mockup: home (incomplete)
    mockup homenya belum selese terus masih kobam warnanya

---
+++ mockup/index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+          
+    <!-- ===========================
+    THEME INFO
+    =========================== -->
+    <meta name="description" content="A free Bootstrap powerd HTML template exclusively crafted for the super lazy designers like me who designed thousand of websites till today but never got a chance to build one himself.">
+    <meta name="keywords" content="Free Portfolio Template, Free Template, Free Bootstrap Template, Dribbble Portfolio Template, Free HTML5 Template">
+    <meta name="author" content="EvenFly Team">
+    
+    <!-- DEVEOPER'S NOTE:
+    This is a free Bootstrap powered HTML template from EvenFly. Feel free to download, modify and use it for yourself or your clients as long there is no money involved.
+    
+    Please don't try to sale it from anywhere; because I want it to be free, forever. If you sale it, That's me who deserves the money, not you :)
+    -->
+
+    <!-- ===========================
+    SITE TITLE
+    =========================== -->
+    <title>Title</title><!-- This is what you see on your browser tab-->
+    
+    <!-- ===========================
+    FAVICONS
+    =========================== -->
+    <link rel="icon" href="img/favicon.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-ipad-retina.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-iphone-retina.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-ipad.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-iphone.png" />
+     
+    <!-- ===========================
+    STYLESHEETS
+    =========================== -->    
+    <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+        
+    <!-- ===========================
+    ICONS: 
+    =========================== -->
+    <link rel="stylesheet" href="css/simple-line-icons.css">    
+    
+    <!-- ===========================
+    GOOGLE FONTS
+    =========================== -->    
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Antic|Raleway:300">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+  <!-- ===========================
+   GOOGLE ANALYTICS (Optional)
+   =========================== -->
+    
+    <!--Replace this line with your analytics code-->
+     
+    <!-- Analytics end-->
+  
+   </head>
+    <body data-spy="scroll">
+        <!-- Preloader -->
+        
+       <header>        
+        <!-- ===========================
+         NAVBAR START
+         =========================== -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                   
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                           <span class="sr-only">Toggle navigation</span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                        </button>
+                        
+                    </div>
+
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav"><!--YOUR NAVIGATION ITEMS STRAT BELOW-->
+                            <li><a href="#news">News</a></li>
+                            <li><a href="#prestasi">Prestasi</a></li>
+                            <li><a href="#event">Acara</a></li>
+                            <li><a href="#gallery">Galeri</a></li>
+                            
+                            <!--don't forget to replace my email address below with yours-->
+                            <li><a href="mailto:m@creatrix.us">Contact</a></li>
+                            
+                        </ul>
+                    </div><!--.nav-collapse -->
+                </div>
+            </nav><!--navbar end-->
+            <!-- ===========================
+            HERO AREA
+            =========================== -->
+            <div id="hero">
+                <div class="overlay">
+                </div>
+
+                <div class="container herocontent">               
+                    <div class="wow fadeInUp" data-wow-duration="2s">
+                        <img id="logo" src="img/hmif.png" alt="">
+                    </div>                
+                    <div class="wow fadeInDown" data-wow-duration="2s">
+                        <h2>Himpunan Mahasiswa Informatika</h2>
+                        <h3>Institut Teknologi Bandung</h3>
+                    </div>
+                </div>
+                
+            </div><!--HERO AREA END-->
+
+        </header><!--header end--> 
+
+        
+    
+    <!-- ===========================
+    NEWS SECTION START
+    =========================== -->
+     <section id="news">
+        <div class="container">
+
+            <div class="sectionhead">
+                <h1>Recent News</h1>
+            </div>
+
+            <div class="sectioncontent">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="news-box">
+                            <div class="news-image">
+                            </div>
+                            <div class="news-content">
+                                <h1>News 1</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit officiis debitis quod ut incidunt quam dolores necessitatibus repudiandae molestias at eius reiciendis ad autem, facilis, delectus sunt consectetur architecto quaerat!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="news-box">
+                            <div class="news-image">
+                            </div>
+                            <div class="news-content">
+                                <h1>News 2</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sed et nihil pariatur facere animi dolorum harum molestias dolore libero magni minus, minima esse doloremque aliquam, laborum, enim, nemo! Facilis.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="news-box">
+                            <div class="news-image">
+                            </div>
+                            <div class="news-content">
+                                <h1>News 3</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nam vitae accusamus odio odit neque dolor, autem voluptate eveniet. Rerum necessitatibus iusto sequi veritatis perspiciatis nesciunt officiis autem harum voluptatem!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="sectionfooter">
+                <button class="btn" id="archive"><span class="glyphicon glyphicon-book"></span> ARCHIVE</button>
+            </div>
+
+        </div>
+     </section><!-- NEWS SECTION END -->
+        
+    <!-- ===========================
+    ACHIEVEMENT SECTION START
+    =========================== -->
+    <section id="achievement" class="section-inverse">
+        <div class="container">
+            
+            <div class="sectionhead">
+                <h1>Achievements</h1>
+            </div>
+            
+        </div>
+    </section><!-- ACHIEVEMENT SECTION END -->
+    
+    <!-- ===========================
+    EVENT SECTION START
+    =========================== -->
+    <section id="event">
+        <div class="container">
+
+            <div class="sectionhead">
+                <h1>Upcoming Events</h1>
+            </div>
+
+        </div>
+    </section><!-- EVENT SECTION END -->
+
+    <!-- ===========================
+    GALLERY SECTION START
+    =========================== -->
+    <section id="gallery" class="section-inverse">
+        <div class="container">
+
+            <div class="sectionhead">
+                <h1>Galeri</h1>
+            </div> 
+
+            <div class="sectioncontent">
+                <div class="row">
+                    <ul class="img-gallery">
+                        
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- TESTIMONIAL SECTION END -->
+       
+    <!-- ===========================
+    FOOTER START
+    =========================== -->    
+    <footer>
+        <div class="container">
+            <span class="bigicon icon-speedometer "></span>
+             
+            <div class="footerlinks"><!-- FOOTER LINKS START -->            
+                <ul>
+                    <li><a href="#hero">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#testimonials">Testimonials</a></li>
+
+                    <!--replace the email address below with your email address-->
+                    <li><a href="mailto:m@creatrix.us">Contact</a></li>                   
+                </ul>
+            </div><!-- FOOTER LINKS END -->
+             
+            <div class="copyright"><!-- FOOTER COPYRIGHT START -->
+                <p><a href="#">Drifolio</a> stands for Dribbble-Portolio. This is a free HTML template designed and coded by <a href="http://evenfly.com">EvenFly Team</a>. All logo and images in this template used for preview purpose only. You are free to use the template but make sure to use your own images.</p>
+            </div><!-- FOOTER COPYRIGHT END -->
+             
+            <div class="footersocial wow fadeInUp" data-wow-duration="3s"><!-- FOOTER SOCIAL ICONS START -->
+                <ul>
+                    <li><a href="http://facebook.com/MamunSrizon"><span class="icon-social-facebook"></span></a></li>
+                    <li><a href="http://twitter.com/MamunSrizon"><span class="icon-social-twitter"></span></a></li>
+                    <li><a href="#"><span class="icon-social-youtube"></span></a></li>
+                    <li><a href="http://dribbble.com/srizon"><span class="icon-social-dribbble"></span></a></li>
+                    <li><a href="#"><span class="icon-social-tumblr"></span></a></li>
+                 </ul>
+             </div><!-- FOOTER SOCIAL ICONS END -->
+         </div>
+     </footer><!-- FOOTER END -->
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    
+    <!--Other necessary scripts-->
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.jribbble-1.0.1.ugly.js"></script>
+    <script src="js/drifolio.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script>new WOW().init();</script>    
+  </body>
+</html>
